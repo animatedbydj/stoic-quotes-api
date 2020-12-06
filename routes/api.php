7d3473b20541012e19554b46f560acf62f8fbf3f
premierconsulting <animatedbydj@gmail.com>
2020-12-06 08:53:22 -0800

    set rate limiting for routes

--- routes/api.php
+++ routes/api.php
@@ -18,17 +18,23 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('quotes', 'App\Http\Controllers\QuoteController@getAllQuotes');
-Route::post('quotes', 'App\Http\Controllers\QuoteController@createQuote');
-Route::put('quotes/{id}', 'App\Http\Controllers\QuoteController@updateQuote');
-Route::delete('quotes/{id}','App\Http\Controllers\QuoteController@deleteQuote');
-Route::get('quotes/random','App\Http\Controllers\QuoteController@getRandomQuote');
-Route::get('quotes/{author}', 'App\Http\Controllers\QuoteController@getQuotesByAuthor');
+
+Route::middleware(['throttle:quotes'])->group(function () {
+    Route::get('quotes', 'App\Http\Controllers\QuoteController@getAllQuotes');
+    Route::post('quotes', 'App\Http\Controllers\QuoteController@createQuote');
+    Route::put('quotes/{id}', 'App\Http\Controllers\QuoteController@updateQuote');
+    Route::delete('quotes/{id}','App\Http\Controllers\QuoteController@deleteQuote');
+    Route::get('quotes/random','App\Http\Controllers\QuoteController@getRandomQuote');
+    Route::get('quotes/{author}', 'App\Http\Controllers\QuoteController@getQuotesByAuthor');
 
 
 // Authors
-Route::get('authors', 'App\Http\Controllers\AuthorController@getAllAuthors');
-Route::post('authors', 'App\Http\Controllers\AuthorController@createAuthor');
+    Route::get('authors', 'App\Http\Controllers\AuthorController@getAllAuthors');
+    Route::post('authors', 'App\Http\Controllers\AuthorController@createAuthor');
+
+});
+
+
 
 
 
